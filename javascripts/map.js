0870ec4b669b3639217de37ea4dd696e1986f448
novelaspalo <novela.spalo@gmail.com>
2017-04-22 04:06:21 +0700

    1.0

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,8 +1,9 @@
 var month = 'january';
 var map;
+
 function initMap() {
     // Create the map.
-     map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
         center: {
             lat: -7.614529,
@@ -17,42 +18,58 @@ function initMap() {
     // Note: We scale the area of the circle based on the population.
     render();
 }
+
 function render() {
-  console.log('render');
-  if(month === 'january')
-  {
-    console.log("january");
-    var citymap = january;
-  }
-  if(month === 'february')
-  {
-    console.log("February");
-    var citymap = february;
-  }
-  for (var city in citymap) {
-      // Add the circle for this city to the map.
-      if (Math.sqrt(citymap[city].population) < 200) {
-          var cityCircle = new google.maps.Circle({
-              strokeColor: '#000000',
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: '#000000',
-              fillOpacity: 0.35,
-              map: map,
-              center: citymap[city].center,
-              radius: 5500
-          });
-      } else {
-          var cityCircle = new google.maps.Circle({
-              strokeColor: '#FF0000',
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: '#FF0000',
-              fillOpacity: 0.35,
-              map: map,
-              center: citymap[city].center,
-              radius: 5500
-          });
-      }
-  }
+    console.log('render');
+    if (month === 'january') {
+        console.log("january");
+        var citymap = january;
+    }
+    if (month === 'february') {
+        console.log("February");
+        var citymap = february;
+    }
+    for (var city in citymap) {
+        // Add the circle for this city to the map.
+        if (Math.sqrt(citymap[city].population) < 200) {
+            var cityCircle = new google.maps.Circle({
+                strokeColor: '#000000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#000000',
+                fillOpacity: 0.35,
+                map: map,
+                center: citymap[city].center,
+                radius: 7500
+            });
+        } else {
+            var cityCircle = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35,
+                map: map,
+                center: citymap[city].center,
+                radius: 7500
+            });
+        }
+
+        var marker = new google.maps.Marker({
+            position: citymap[city].center,
+            map: map
+        });
+        var message = "<p> Kota/Kab: " + citymap[city].name + "</p>"+ "<p>  Jumlah User:" + citymap[city].population + "</p>";
+        attachSecretMessage(marker, message);
+    }
+
+    function attachSecretMessage(marker, message) {
+        var infowindow = new google.maps.InfoWindow({
+            content: message
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(marker.get('map'), marker);
+        });
+    }
 }
