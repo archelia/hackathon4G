7965e5236b6011c0d8716b825192b24067e4235b
novelaspalo <novela.spalo@gmail.com>
2017-04-21 14:54:56 +0700

    new

---
+++ javascripts/january.js
@@ -0,0 +1,58 @@
+var citymap = {
+  Badung: {
+    center: {
+      lat: -8.581930,
+      lng: 115.177059
+    },
+    population: 20637
+  },
+  Bangli: {
+    center: {
+      lat: -8.581930,
+      lng: 115.177059
+    },
+    population: 1941
+  },
+  Buleleng: {
+    center: {
+      lat: -8.581930,
+      lng: 115.177059
+    },
+    population: 42332
+  },
+  JakartaSelatan: {
+    center: {
+      lat: -6.261493,
+      lng: 106.810600
+    },
+    population: 260747
+  },
+  JakartaUtara: {
+    center: {
+      lat: -6.138377,
+      lng: 106.866466
+    },
+    population: 260747
+  },
+  JakartaTimur: {
+    center: {
+      lat: -6.225014,
+      lng: 106.900447
+    },
+    population: 195663
+  },
+  JakartaBarat: {
+    center: {
+      lat: -6.168329,
+      lng: 106.758849
+    },
+    population: 123962
+  },
+  JakartaPusat: {
+    center: {
+      lat: -6.186486,
+      lng: 106.834091
+    },
+    population: 164655
+  }
+};
